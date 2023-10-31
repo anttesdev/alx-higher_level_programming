b314c06311b035436b71141f8e416d1371de429b
Anteneh <anttesdev>
2023-10-31 14:09:37 -0700

    uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    new_str = ""
     for c in str:
         if 97 <= ord(c) <= 122:
-            upper = chr(ord(c) - 32)
-            new_str += upper
-        else:
-            new_str += c
-    print(new_str, end="\n")
+            c = chr(ord(c) - 32)
+        print("{:s}".format(c), end="")
+    print()
