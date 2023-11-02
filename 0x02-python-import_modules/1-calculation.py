7583b406181c6088c4121d169d1ed1dd96e91bed
Anteneh <anttesdev>
2023-11-02 06:27:05 -0700

    import calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     b = 5
     result = add(a, b)
     dif = sub(a, b)
-    pro =  mul(a, b)
+    pro = mul(a, b)
     quo = div(a, b)
     print("{} + {} = {}".format(a, b, result))
     print("{} - {} = {}".format(a, b, dif))
