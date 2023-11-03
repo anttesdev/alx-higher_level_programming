fe042ae45260056ae7afa03a028c1f92d7eb468d
Anteneh <anttesdev>
2023-11-03 09:17:23 -0700

    change copied list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    my_list_cp = my_list.copy()
+    if idx < 0:
+        return (my_list_cp)
+    if idx > len(my_list) - 1:
+        return (my_list_cp)
+    else:
+        my_list_cp[idx] = element
+        return (my_list_cp)
