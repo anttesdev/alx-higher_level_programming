ebaaa80867e775918269edaa80fcc3f972c309a4
Anteneh <anttesdev>
2023-11-04 00:51:55 -0700

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     my_list_cp = my_list.copy()
     if idx < 0:
         return (my_list_cp)
-    if idx > len(my_list) - 1:
+    if idx >= len(my_list):
         return (my_list_cp)
     else:
         my_list_cp[idx] = element
