6bbd99bf54da3539a32eb7a21310dd0d1c4b1ad4
Anteneh <anttesdev>
2023-11-21 17:18:52 -0800

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -63,9 +63,9 @@ class Square:
             value: the value for the position
         """
         if (not isinstance(value, tuple)) or \
-        (len(value) != 2) or \
-        any((type(num) is not int) for num in value) or \
-        not all(num >= 0 for num in value):
+                (len(value) != 2) or \
+                any((type(num) is not int) for num in value) or \
+                not all(num >= 0 for num in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
