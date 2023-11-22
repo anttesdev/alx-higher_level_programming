5a7834613d4293fea3ffa35c72c5d498a0625d97
Anteneh <anttesdev>
2023-11-21 17:13:01 -0800

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -91,5 +91,5 @@ class Square:
         for j in range(self.__position[1]):
             print()
         for i in range(self.__size):
-            print("_" * self.__position[0], end="")
+            print(" " * self.__position[0], end="")
             print("#" * self.__size)
