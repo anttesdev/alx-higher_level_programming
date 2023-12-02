9e34469be3981e5cc156d619aeebc52cd837ad40
Anteneh <anttesdev@gmail.com>
2023-12-01 16:49:09 -0800

    100-matrix_mul.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -6,14 +6,14 @@ def matrix_mul(m_a, m_b):
     if not all(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
 
-    if not m_a or not all(row for row in m_a):
+    if not m_a or any(not row for row in m_a):
         raise ValueError("m_a can't be empty")
 
-    if not all(isinstance(num, (int, float)) for row in m_a for num in row):
+    if any(not isinstance(element, (int, float)) for row in m_a for element in row):
         raise TypeError("m_a should contain only integers or floats")
 
-    if not all(len(row) == len(m_a[0]) for row in m_a):
-        raise TypeError("each row of m_a must be of the same size")
+    if len(set(len(row) for row in m_a)) != 1:
+        raise TypeError("Each row of m_a must be of the same size")
 
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
@@ -21,17 +21,18 @@ def matrix_mul(m_a, m_b):
     if not all(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
 
-    if not m_b or not all(row for row in m_b):
+    if not m_b or any(not row for row in m_b):
         raise ValueError("m_b can't be empty")
 
-    if not all(isinstance(num, (int, float)) for row in m_b for num in row):
+    if any(not isinstance(element, (int, float)) for row in m_b for element in row):
         raise TypeError("m_b should contain only integers or floats")
 
-    if not all(len(row) == len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_b must be of the same size")
+    if len(set(len(row) for row in m_b)) != 1:
+        raise TypeError("Each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
     result = [[sum(a * b for a, b in zip(row_a, col_b)) for col_b in zip(*m_b)] for row_a in m_a]
+
     return result
