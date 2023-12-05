a32aa1043f02e29d5ba9a26254856f0e6dacd5a8
Anteneh <anttesdev@gmail.com>
2023-12-05 10:06:17 -0800

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+A function that writes text in to a file
+and returns the number of Chars in the text
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Function that writes text to a file
+    Args:
+        filename: the name of the file
+        text(str): the text to be written to the file
+    Returns: the number of Chars in the file
+    """
+    with open("filename", 'w', encoding='utf-8') as file:
+        file.write(text)
+        return len(text)
