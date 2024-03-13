7552815fdb3b19adfbaf67f0412fbb1fe6b14013
Anteneh <anttesdev@gmail.com>
2024-03-12 17:15:43 -0700

    1011

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- Select and display all records from the table second_table, ordered by score in descending order
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Select and display records with a score >= 10 from the table second_table, ordered by score in descending order
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+
