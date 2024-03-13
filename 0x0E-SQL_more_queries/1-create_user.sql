5af5b927c9e53352b076d1f23183f4884b2c5d85
Anteneh <anttesdev@gmail.com>
2024-03-13 14:42:04 -0700

    1-create_user.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Create user user_0d_1 with all privileges and set password
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
