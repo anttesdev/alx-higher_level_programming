bdadf21ebece6620084b8a06b4bd04909e2e99b5
Anteneh <anttesdev@gmail.com>
2024-03-12 17:50:08 -0700

    101-avg_temperatures.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,7 @@
 -- Import the table dump into the hbtn_0c_0 database
 
 -- Write a script to display the average temperature by city in Fahrenheit
-SELECT city, AVG(temperature * 9/5 + 32) AS avg_temperature_fahrenheit
+SELECT city, AVG(value) AS avg_temprature
 FROM weather_data
 GROUP BY city
 ORDER BY avg_temperature_fahrenheit DESC;
