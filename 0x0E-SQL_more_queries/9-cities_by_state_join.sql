4821e8e4859b0496c89298b53e2589336054d49e
Anteneh <anttesdev@gmail.com>
2024-03-14 15:34:42 -0700

    9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
-- Selects all cities along with their corresponding state names and orders them by city ID.
+-- Selects all cities along with their corresponding state names and orders them by city ID.
 SELECT cities.id, cities.name, states.name
-FROM cities, states
-WHERE cities.state_id = states.id
+FROM cities
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
