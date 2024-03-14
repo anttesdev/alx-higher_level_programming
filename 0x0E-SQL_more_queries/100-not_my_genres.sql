103753fdb1cb19ca65b1534d9a4849dfce150345
Anteneh <anttesdev@gmail.com>
2024-03-14 15:56:50 -0700

    16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,4 +1,4 @@
--- Query to list all genres not linked to the show Dexter from hbtn_0d_tvshows
+-- Query to list all genres not linked to the show Dexter
 SELECT tv_genres.name
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
